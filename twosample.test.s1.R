1efae2922a3333c3b954f5ecadf8093667c6e294
Javier González-Delgado <88046156+gonzalez-delgado@users.noreply.github.com>
2022-01-31 14:45:53 +0100

    Add files via upload

--- twosample.test.s1.R
+++ twosample.test.s1.R
@@ -19,7 +19,7 @@
 #'
 #' @examples
 #' 
-#' n <- 30 # Sample size
+#' n <- 50 # Sample size
 #' 
 #' # Simulate the statistic null distribution
 #' NR <- 100
@@ -27,22 +27,8 @@
 #'  
 #'  x <- runif(n, 0,1)
 #'  y <- runif(n, 0,1)
-#'  
-#'  cut_point<-find.cutpoint.s1(x, y, messages = FALSE)$cut_point #Optimal origin
-#'  
-#'  x_left <- sort(x[which(x < cut_point)])
-#'  x_right <- sort(x[which(x >= cut_point)])
-#'  x_new <- c(x_right,x_left+1)
-#'  x_new <- x_new - cut_point 
-#'  
-#'  y_left <- sort(y[which(y < cut_point)])
-#'  y_right <- sort(y[which(y >= cut_point)])
-#'  y_new <- c(y_right, y_left + 1)
-#'  y_new <- y_new - cut_point 
-#'  
-#'  statistic <- stat.s1(x_new, y_new) 
-#'  
-#'  statistic
+#'  stat.s1(x, y) 
+#'
 #' })
 #' 
 #' x <- runif(n, 0, 1)
